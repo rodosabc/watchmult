a13b716ae0058809513d4d14ce944c8301883e93
rodosabc <rodosabc@gmail.com>
2016-07-25 20:20:03 +0300

    Seeds for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_nav_categories
-    @categories = Category.where(:category_id => nil)
+    @categories = Category.where(:parent_id => nil)
   end
 end
